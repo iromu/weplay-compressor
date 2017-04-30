d8dec0ea3e1a2258f7c669cf11ef4a76d2f40e28
Iván Rodríguez Murillo <wantez@gmail.com>
2017-04-30 11:33:45 +0200

    WIP

--- CompressorService.js
+++ CompressorService.js
@@ -1,195 +1,102 @@
-const uuid = require('node-uuid').v4();
-const logger = require('weplay-common').logger('compressor-service', uuid);
-const EventBus = require('weplay-common').EventBus;
+const uuid = require('node-uuid').v4()
+const logger = require('weplay-common').logger('compressor-service', uuid)
+const EventBus = require('weplay-common').EventBus
+const fps = require('fps')
+
+const autoload = process.env.AUTOLOAD || false
 
 class CompressorService {
+  constructor(discoveryUrl, discoveryPort) {
+    this.uuid = require('node-uuid').v4()
+    this.pngquant = undefined
+    this.failures = 0
+    this.romHash = undefined
+    this.ticker = fps({every: 10})
+    this.ticker.on('data', framerate => {
+      logger.info('CompressorService[%s] fps %s', uuid, framerate)
+    })
+    this.bus = new EventBus({
+      url: discoveryUrl,
+      port: discoveryPort,
+      name: 'compressor',
+      id: this.uuid,
+      serverListeners: {
+        'streamJoinRequested': this.streamJoinRequested.bind(this),
+        'streamCreateRequested': this.streamCreateRequested.bind(this)
+      }
+    }, () => {
+      logger.info('CompressorService connected to discovery server', {
+        discoveryUrl: discoveryUrl,
+        uuid: this.uuid
+      })
+      this.init()
+    })
+  }
 
-    constructor(discoveryUrl, discoveryPort) {
-        this.uuid = require('node-uuid').v4();
-        this.pngquant = undefined;
-        this.failures = 0;
-        this.sockets = [];
-
-        this.bus = new EventBus({
-            url: discoveryUrl,
-            port: discoveryPort,
-            name: 'compressor',
-            id: this.uuid,
-            serverListeners: {
-                'frame': this.onRawFrame.bind(this),
-                'stream:join': this.onStreamJoin.bind(this),
-            },
-            listeners: [
-                //{name: 'emu', event: 'frame', handler: this.onRawFrame.bind(this)},
-                {name: 'game', event: 'join', handler: this.onGameJoin.bind(this)}
-            ]
-        }, ()=> {
-            logger.info('CompressorService connected to discovery server', {
-                discoveryUrl: discoveryUrl,
-                uuid: this.uuid
-            });
-            this.init();
-        });
+  init() {
+    if (autoload) {
+      //this.bus.emit('rom', 'request');
     }
 
-    init() {
-        try {
-            // compression lib
-            this.pngquant = require('node-pngquant-native');
-            logger.debug('pngquant loaded');
-        } catch (e) {
-            logger.error(e);
-        }
+    try {
+      this.romHash = null
+      // compression lib
+      this.pngquant = require('node-pngquant-native')
+      logger.debug('pngquant loaded')
+      logger.info('CompressorService waiting for incoming users.')
+    } catch (e) {
+      logger.error(e)
     }
+  }
 
-    onRawFrame(socket, data) {
-        const hash = data.hash;
-        const frame = data.frame;
-
-        if (this.pngquant && this.failures < 3) {
-            try {
-                const resBuffer = this.pngquant.compress(frame);
-                this.sendFrame(hash, resBuffer);
-            } catch (e) {
-                this.failures++;
-                this.sendFrame(hash, frame);
-            }
-        } else {
-            this.sendFrame(hash, frame);
-        }
+  onRawFrame(frame) {
+    if (this.pngquant && this.failures < 3) {
+      try {
+        const resBuffer = this.pngquant.compress(frame)
+        this.sendFrame(resBuffer)
+      } catch (e) {
+        this.failures++
+        this.sendFrame(frame)
+      }
+    } else {
+      this.sendFrame(frame)
     }
+  }
 
-    onGameJoin(socket, user) {
-        logger.debug('onGameJoin. Choose me !!!!', user);
-        //{nick: socket.nick, hash: hash, clientId: clientId}
+  streamCreateRequested(socket, request) {
+    logger.info('CompressorService.streamCreateRequested', {
+      socket: socket.id,
+      request: JSON.stringify(request)
+    })
+  }
 
-        // Given the user.hash selection,
-        // Then return where it should listen for incoming frames
-
-        socket.emit('frame:location', this.uuid);
+  streamJoinRequested(socket, request) {
+    logger.info('CompressorService.streamJoinRequested', {
+      socket: socket.id,
+      request: JSON.stringify(request)
+    })
+    if (!this.romHash || this.romHash === request) {
+      this.romHash = request
+      socket.join(this.romHash)
+      // Locate a raw frame stream supplier
+      // channel, room, event, listener
+      this.bus.streamJoin('emu', this.romHash, 'frame', this.onRawFrame.bind(this))
+      //this.bus.stream(this.romHash, 'frame', {});
+    } else {
+      logger.error('EmulatorService.streamJoinRequested. Ignoring request for a new stream.', {
+        socket: socket.id,
+        request: JSON.stringify(request)
+      })
     }
+  }
 
-    // socket:  GatewayService.js
-    // user:    {nick: socket.nick, hash: hash, clientId: clientId}
-    onStreamJoin(socket, user) {
-        logger.debug('onStreamJoin.', {user: user});
-        //{nick: socket.nick, hash: hash, clientId: clientId}
-        if (!socket.hashes) {
-            socket.hashes = [];
-        }
-        socket.hashes.push(user.hash);
-        // Given the user.hash selection,
-        // Then find a emulator, join and relay frame data
-        // on private channel
-        socket.join(`${user.hash}:frame`);
-        if (this.sockets.filter(s=>s.id === socket.id)[0] === undefined) {
-            this.sockets.push(socket);
-        }
-    }
+  sendFrame(frame) {
+    this.ticker.tick()
+    this.bus.stream(this.romHash, 'frame', frame)
+  }
 
-    sendFrame(room, frame) {
-        //logger.debug('sendFrame.', {room: room});
-        this.bus.publish(`${room}:frame`, frame);
-        /*this.sockets
-         //.filter(s=>s.hash === room)
-         .forEach((socket)=> {
-         //logger.debug(`sendFrame to ${room}:frame`, {socket: socket.id});
-         socket.emit(`${room}:frame`, frame);
-         })*/
-    }
-
-    destroy() {
-    }
+  destroy() {
+  }
 }
 
-
-module.exports = CompressorService;
-//
-//return;
-//// redis
-//const redis = require('weplay-common').redis();
-//const redisSub = require('weplay-common').redis();
-//
-//const EventBus = require('weplay-common').EventBus;
-//const bus = new EventBus(redis, redisSub);
-//
-//const io = require('socket.io-emitter')(redis);
-//
-//const fps = require('fps');
-//const ticker = fps({
-//    every: 100   // update every 10 frames
-//});
-//ticker
-//    .on(
-//        'data'
-//        ,
-//        framerate
-//            => {
-//            logger
-//                .info(
-//                    'fps'
-//                    , {
-//                        fps: Math.round
-//                        (
-//                            framerate
-//                        )
-//                    }
-//                )
-//            ;
-//        })
-//;
-//
-//let pngquant;
-//try {
-//    // compression lib
-//    pngquant = require('node-pngquant-native');
-//    logger.info('pngquant loaded');
-//} catch (e) {
-//    logger.error(e);
-//}
-//
-//let failures = 0;
-//const persistCounter = {};
-//const sendFrame = (room, frame) => {
-//    io.to(room).emit('frame', frame);
-//    const counter = persistCounter[room];
-//    if (counter > 10 || counter === undefined || counter === 0) {
-//        persistCounter[room] = 1;
-//        redis.set(`weplay:frame:${room}`, frame);
-//    } else {
-//        persistCounter[room] = counter + 1;
-//    }
-//};
-//
-//
-//bus.psubscribe('weplay:frame:raw:*', (pattern, channel, frame) => {
-//    const room = channel.toString().split(":")[3];
-//
-//    if (pngquant && failures < 3) {
-//        try {
-//            const resBuffer = pngquant.compress(frame);
-//            sendFrame(room, resBuffer);
-//        } catch (e) {
-//            failures++;
-//            logger.error('weplay:frame:raw', e);
-//            sendFrame(room, frame);
-//        }
-//    } else {
-//        logger.debug('weplay:frame:raw');
-//        sendFrame(room, frame);
-//    }
-//    ticker.tick();
-//});
-//
-//
-//require('weplay-common').cleanup(function destroyData() {
-//    logger.info('Destroying data.');
-//    for (var room in persistCounter) {
-//        if (persistCounter.hasOwnProperty(room)) {
-//            redis.del(`weplay:frame:${room}`);
-//        }
-//    }
-//
-//    bus.publish('weplay:compressor:unsubscribe', uuid);
-//    bus.destroy();
-//});
+module.exports = CompressorService
